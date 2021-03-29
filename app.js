d90851c5d63fcfcbed6aa44964bf66be9cefd208
JonhParra <jonathan.parra@corebiz.ag>
2021-03-29 10:02:33 -0600

    Update/subiendo cambios y avance en operaciones

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const btnCategorias = document.getElementById('btn-categorias');
 const btnBalance = document.getElementById('btn-balance');
-
 const balance = document.getElementById('balance');
 const categorias = document.getElementById('categorias');
 const btnNuevaOperacion = document.getElementById('btn-agregaroperacion');
+const btnAgregar = document.getElementById('btn-agregar');
+
+
+const descripcion = document.getElementById('descripcion');
+const monto = document.getElementById('monto');
+const tipo = document.getElementById('tipo');
+const categoria = document.getElementById('categoria');
 const inputFecha = document.getElementById('input-fecha');
+const pintar = document.getElementById('pintar');
 
 
 btnBalance.addEventListener('click', () => {
@@ -25,14 +32,50 @@ const day = new Date().getDate();
 let month = new Date().getMonth() + 1;
 const year = new Date().getFullYear();
 
-console.log(new Date().getDate());
-console.log(new Date().getMonth() + 1);
-console.log(new Date().getFullYear());
-
-console.log(new Date().getMonth() < 10);
 
 inputFecha.value = `${year}-${month < 10 ? '0' + month: month}-${day}`
-// yyyy-MM-dd
-// yyyy-M-dd
+
+
+
+
+let operaciones = [];
+
+const pintarOperaciones = (operaciones) => {
+  pintar.innerHTML = '';
+  for (let index = 0; index < operaciones.length; index++) {
+    const caja = 
+    `<div>
+      <span>${operaciones[index].descripcion}</span>
+      <span>${operaciones[index].monto}</span>
+      <span>${operaciones[index].tipo}</span>
+      <span>${operaciones[index].categoria}</span>
+      <span>${operaciones[index].fecha}</span>
+    </div>`
+    pintar.insertAdjacentHTML('beforeend', caja)
+  }
+}
+
+
+btnAgregar.addEventListener('click', () => {
+  operaciones = JSON.parse(localStorage.getItem('operaciones'))
+  const nuevaOperacion = {
+    descripcion: descripcion.value,
+    monto: monto.value,
+    tipo: tipo.value,
+    categoria: categoria.value,
+    fecha: inputFecha.value
+  }
+  operaciones.push(nuevaOperacion)
+  localStorage.setItem('operaciones', JSON.stringify(operaciones))
+  operacionesLocalStorage = JSON.parse(localStorage.getItem('operaciones'))
+  pintarOperaciones(operacionesLocalStorage)
+})
+
+let operacionesLocalStorage = JSON.parse(localStorage.getItem('operaciones'))
+pintarOperaciones(operacionesLocalStorage)
+
+
+
+
 
 
